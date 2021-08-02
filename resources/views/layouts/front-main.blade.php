7c82bb8511a558f7458ddd019a5543f7e9bfb54c
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2021-08-02 12:47:25 +0700

    siap upload

--- resources/views/layouts/front-main.blade.php
+++ resources/views/layouts/front-main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <meta name="description" content="Elearning STIE Cendekia Karya Utama Semarang">
+    <meta name="keywords" content="moodle, Elearning Cendekiaku" />
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
